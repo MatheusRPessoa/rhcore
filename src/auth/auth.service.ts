5b435598f6fe2097cfd45d41fcc709c5c91ad4a3
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-29 07:52:48 -0300

    feat: create method login in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(username: string, password: string) {
+    const user = await this.usersService.validateCredentials(
+      username,
+      password,
+    );
+    const tokens = await this.generateTokens(user.ID, user.NOME_USUARIO);
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  private async generateTokens(userId: string, username: string) {}
 }
