f136c0cfae8a0a2b058c8333f596d6d133fd98c7
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-29 08:00:52 -0300

    feat: add data source in database config

---
+++ src/config/database/data-source.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+dotenv.config({ path: envFile });
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../../**/*.entity.{ts,js}'],
+  migrations: [__dirname + '/migrations/*.{ts,js}'],
+  synchronize: false,
+});
