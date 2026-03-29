47d87c71165b2cde4e749e229e0dd42577dfca0e
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-29 15:07:25 -0300

    feat(users): add CRUD methods to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -10,6 +16,34 @@ export class UsersService {
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    const hashed = await bcrypt.hash(dto.SENHA, 10);
+    const user = this.usersRepository.create({ ...dto, SENHA: hashed });
+    return this.usersRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { ID: id } });
+
+    if (!user) throw new NotFoundException('Usuário não encontrado');
+    return user;
+  }
+
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
+    const user = await this.findOne(id);
+    Object.assign(user, dto);
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    await this.usersRepository.remove(user);
+  }
 
   async validateCredentials(username: string, password: string): Promise<User> {
     const user = await this.usersRepository.findOne({
