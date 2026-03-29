ab3cbc8d70bf32899e330b2a4346cd3d143b0c41
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-29 08:05:24 -0300

    refactor: create validateCredentials, findForAuth an updateRefreshToken methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async validateCredentials(username: string, password: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: { NOME_USUARIO: username },
+    });
+
+    if (!user) throw new UnauthorizedException('Credenciais inválidas');
+
+    const passwordMatch = await bcrypt.compare(password, user.SENHA);
+    if (!passwordMatch)
+      throw new UnauthorizedException('Credenciais inválidas');
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findForAuth(userId: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { ID: userId } });
+    if (!user) throw new UnauthorizedException('Usuário não encontrado');
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async updateRefreshToken(
+    userId: string,
+    refreshToken: string | null,
+  ): Promise<void> {
+    await this.usersRepository.update(userId, { REFRESH_TOKEN: refreshToken });
   }
 }
