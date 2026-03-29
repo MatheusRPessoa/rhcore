247bac616bf063b88784d8f25fbb49284c7fd322
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-29 15:12:54 -0300

    feat: add create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'admin123' })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  SENHA: string;
+}
