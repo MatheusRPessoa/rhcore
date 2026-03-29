57baa974c0bc2b23ada56204a98a031694f3946c
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-29 08:06:53 -0300

    refactor: complete User entity implementation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,31 @@
-import { BaseEntity, Column, Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from 'src/common/entities/base.entity';
 
 @Entity('USUARIOS')
-export class User extends BaseEntity {}
+export class User extends BaseEntity {
+  @Column({
+    name: 'NOME_USUARIO',
+    length: 255,
+  })
+  NOME_USUARIO: string;
+
+  @Column({
+    name: 'EMAIL',
+    length: 255,
+    unique: true,
+  })
+  EMAIL: string;
+
+  @Column({
+    name: 'SENHA',
+    length: 255,
+  })
+  SENHA: string;
+
+  @Column({
+    name: 'REFRESH_TOKEN',
+    length: 500,
+    nullable: true,
+  })
+  REFRESH_TOKEN: string | null;
+}
