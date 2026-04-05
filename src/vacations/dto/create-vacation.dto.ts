ae4b4742f22b06e338a47d6548627e485ba8a52c
Matheus Pessoa <matheus41.dev@gmail.com>
2026-04-05 16:48:51 -0300

    feat: add dtos in vacations module

--- src/vacations/dto/create-vacation.dto.ts
+++ src/vacations/dto/create-vacation.dto.ts
@@ -1,1 +1,32 @@
-export class CreateVacationDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateVacationDto {
+  @ApiProperty({ example: 'a3bb189e-8bf9-3888-9912-ace4e6543002' })
+  @IsUUID('4', { message: 'O ID do funcionário deve ser um UUID válido' })
+  @IsNotEmpty({ message: 'O ID do funcionário é obrigatório' })
+  FUNCIONARIO_ID: string;
+
+  @ApiProperty({ example: '2025-07-30' })
+  @IsDateString({}, { message: 'A data de inicio deve ser uma data válida' })
+  @IsNotEmpty({ message: 'A data de inicio é obrigatória' })
+  DATA_INICIO: string;
+
+  @ApiProperty({ example: '2025-07-30' })
+  @IsDateString({}, { message: 'A data de fim deve ser uma da válida' })
+  @IsNotEmpty({ message: 'A data de fim é obrigatória' })
+  DATA_FIM: string;
+
+  @ApiPropertyOptional({ example: 'Férias programadas do segundo semestre' })
+  @IsString({ message: 'A observação deve ser do tipo texto' })
+  @IsOptional()
+  @MaxLength(500, { message: 'A observação deve ter ' })
+  OBSERVACAO?: string;
+}
